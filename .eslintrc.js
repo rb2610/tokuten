4fa95809c6cf71e233bcea1aa9ce1fbc7ea62537
rb2610 <rb2610@hotmail.co.uk>
2018-10-03 22:47:24 +0100

    Working server tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
         "indent": [
             "error",
             "spaces",
-            "4"
+            "2"
         ],
         "linebreak-style": [
             "error",
