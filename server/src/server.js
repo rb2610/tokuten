1f4a0f778d5e053ebd1db40913d9ead3a1eda363
rb2610 <rb2610@hotmail.co.uk>
2018-10-03 14:42:31 +0100

    Refactor to server/client. Set up test infrastructure for server.

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,10 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
 const port = process.env.port || 3001;
 
 app.get('/api/hello', (request, response) => {
-    response.send({ 'message': 'MANGOES (are great)' });
+    response.send({ 'message': 'MANGOES (は美味しいよ)' });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
